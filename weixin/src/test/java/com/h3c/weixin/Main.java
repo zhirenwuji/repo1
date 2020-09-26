40bb406f04206c3436b34e3a9c75f4df923d536a
tom <2411985666@qq.com>
2020-09-26 23:30:35 +0800

    提交5

--- weixin/src/test/java/com/h3c/weixin/Main.java
+++ weixin/src/test/java/com/h3c/weixin/Main.java
@@ -18,6 +18,7 @@ public class Main {
             }
         }
         System.out.println("uuuuuuuuuu");
+        System.out.println("uuuuuuuuuu");
 
         System.out.println(Arrays.toString(arr));
         System.out.println("++++++++++");
