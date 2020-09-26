6fd72cbce62290293a247c4dd292da0732538898
tom <2411985666@qq.com>
2020-09-26 23:37:42 +0800

    提交微信main6

--- weixin/src/test/java/com/h3c/weixin/Main.java
+++ weixin/src/test/java/com/h3c/weixin/Main.java
@@ -19,6 +19,7 @@ public class Main {
         }
         System.out.println("uuuuuuuuuu");
         System.out.println("uuuuuuuuuu");
+        System.out.println("uuuuuuuuuu");
 
         System.out.println(Arrays.toString(arr));
         System.out.println("++++++++++");
