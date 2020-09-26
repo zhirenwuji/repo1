41664d777919599b2f5c3c0421851be4fb1ac04f
tom <2411985666@qq.com>
2020-09-26 22:28:03 +0800

    提交微信main

---
+++ weixin/src/test/java/com/h3c/weixin/Main.java
@@ -0,0 +1,28 @@
+package com.h3c.weixin;
+
+
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] arr = {4,2,5,1,9,3};
+        for (int i = 1; i < arr.length-1; i++) {
+            for (int j = i-1; j >= 0; j++) {
+                int temp = arr[j];
+                if (arr[j]>arr[j+1]){
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        System.out.println("---");
+
+        System.out.println(Arrays.toString(arr));
+
+
+    }
+
+
+}
